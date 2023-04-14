b1b71fb2a217ff023a94c18e167f9e3ecc13b5b0
Luís Eduardo <luiseduardozordanmacagnan@gmail.com>
2023-04-14 14:20:44 -0300

    Update

--- app/src/main/java/br/com/ifsc/app/MainActivity.java
+++ app/src/main/java/br/com/ifsc/app/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.ifsc.app;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -7,26 +9,42 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static ArrayList<String> listaPalavras;
+    public static ArrayList<String> listaApps;
+    PackageManager pm;
     ListView listView;
     EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listaPalavras=new ArrayList<>();
-        listaPalavras.add("Sugoma");
-        listaPalavras.add("Amogus");
+        listaApps = new ArrayList<>();
 
         listView = findViewById(R.id.lista);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+        pm = getPackageManager();
+
+        List<ApplicationInfo> apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for (ApplicationInfo app: apps) {
+            listaApps.add(app.loadLabel(pm).toString());
+        }
+        //Padrao
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
                 android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                listaPalavras);
-        listView.setAdapter(adapter);
+                listaApps);
+
+        //Personalizado
+        AplicativosAdapter adapter2 = new AplicativosAdapter(
+            this,
+                R.layout.lista,
+                apps
+        );
+
+        //Setando
+        listView.setAdapter(adapter2);
     }
 }
